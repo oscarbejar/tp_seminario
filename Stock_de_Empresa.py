8872f1299f0b923e4271ec2245c3aebe99772c4e
OscarBejar <oskrbejar@gmail.com>
2020-10-22 19:34:21 -0300

    dicy

--- Stock_de_Empresa.py
+++ Stock_de_Empresa.py
@@ -1,1 +1,13 @@
-print("hola")
+print("Stock de Empresa")
+
+producto = input("Ingrese el producto a incluir: ")
+nombreProd = input("Ingrese nombre del producto: ")
+cantidadProd = input("Ingrese cantidad de productos a incluir: ")
+
+stock  = dict()
+
+stock [producto] = {nombreProd,cantidadProd}
+
+for s in stock:
+    print(s)
+    print(stock[s])
