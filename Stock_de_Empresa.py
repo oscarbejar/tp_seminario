eb791343073c75ca3955d6cb04764dbcb130b7c1
OscarBejar <oskrbejar@gmail.com>
2020-10-22 19:53:01 -0300

    commitn

--- Stock_de_Empresa.py
+++ Stock_de_Empresa.py
@@ -1,13 +1,26 @@
+print("_"*50)
+print("-"*50)
 print("Stock de Empresa")
+print("_"*50)
+print("-"*50)
 
-producto = input("Ingrese el producto a incluir: ")
-nombreProd = input("Ingrese nombre del producto: ")
-cantidadProd = input("Ingrese cantidad de productos a incluir: ")
+cargador = True
+while cargador == True:
 
-stock  = dict()
+    producto = input("Ingrese el producto a incluir: ")
+    marcaProd = input("Ingrese marca del producto: ")
+    nombreProd = input("Ingrese nombre del producto: ")
+    cantidadProd = int(input("Ingrese cantidad de productos a incluir: "))
 
-stock [producto] = {nombreProd,cantidadProd}
+    stock  = dict()
+
+    stock[producto] = {marcaProd,nombreProd,cantidadProd}
+
+    resp = input("Desea agregar un producto?(y/n):")
+    if resp != "y":
+        cargador = False
 
 for s in stock:
-    print(s)
+
+    print("El stock de producto ", s)
     print(stock[s])
