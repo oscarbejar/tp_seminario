cac0533ff508d178ed855589a4d25a7fe3266357
OscarBejar <oskrbejar@gmail.com>
2020-11-01 20:11:01 -0300

    cambio

--- Inventario.py
+++ Inventario.py
@@ -3,5 +3,5 @@ def inventario():
 articulos = {
         1:{"marca":"Nike","modelo":"remera","color":"azul","precio":100,"cantidad":1}
     }
-
+    #{"marca":"Nike","modelo":"remera","color":"azul","precio":100,"cantidad":1}
 #f
